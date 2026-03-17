6b94244103d4f04433a408c90616e67e0c2a3cd6
Ganusya <ganusya.ua@gmail.com>
2026-03-17 21:33:11 +0200

    add files-js

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-function';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+  const query = event.target.elements['search-text'].value.trim();
+  if (!query) {
+    iziToast.error({
+      message: 'Search field cannot be empty',
+      position: 'topRight',
+    });
+    return;
+  }
+  clearGallery;
+  showLoader;
+  try {
+    const data = await getImagesByQuery(query);
+    console.log(data);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+    createGallery(data.hits);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
+});
